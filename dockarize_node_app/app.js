968a1838a20272b6b00fabf53dfed16a5f17f60c
Pavithra Sandamini <84126210+Pavi99sandamini151@users.noreply.github.com>
2023-02-05 08:33:24 +0530

    dockarized node app

---
+++ dockarize_node_app/app.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/', (req,res) => res.send('hello world'));
+
+app.listen(port,
+    () => console.log(`App listnenig at http://localhost:${port}`));
